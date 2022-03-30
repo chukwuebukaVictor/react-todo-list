e7428415484943120553d2f0fbed0d12b501f3f8
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2022-03-30 17:23:34 +0100

    Add component for each todo

---
+++ src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import React, { Component } from 'react'
+
+class TodoItem extends Component {
+  render() {
+    return (
+      <div>TodoItem</div>
+    )
+  }
+}
+
+export default TodoItem;
